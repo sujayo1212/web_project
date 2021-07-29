aabb646fdcb668ebb5fbac6d6bd89e10f57ec8c0
sujayo1212 <sujayo1212@gmail.com>
2021-07-29 17:09:30 +0900

    jaeyun

--- management/urls.py
+++ management/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('common/', include('common.urls')),
     # path('lecture/', include('lecture.urls')),
-    # path('my_page/', include('my_page.urls')),
+    path('my_page/', include('my_page.urls')),
     # path('qna/', include('qna.urls')),
 ]
---
+++ my_page/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name='my_page'
+urlpatterns = [
+    path('my_page/', views.my_page, name='my_page'),
+]
