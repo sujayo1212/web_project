b6634c99c2e83d5e571a9a51ea6c4207c7b7cc98
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-07-29 16:57:30 +0900

    create model and CRUD in lecture

--- lecture/models.py
+++ lecture/models.py
@@ -5,8 +5,11 @@ class Lecture(models.Model):
     class_name = models.CharField(max_length=100)
     subject = models.CharField(max_length=50)
     detail_subject = models.CharField(max_length=50)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
     def __str__(self):
         return self.class_name
+
+    def get_absolute_url(self):
+        return f'/lecture/{self.pk}/'
--- lecture/views.py
+++ lecture/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Lecture
+from .forms import LectureForm
 
-# Create your views here.
+
+class LectureList(ListView):
+    model = Lecture
+
+
+class LectureDetail(DetailView):
+    model = Lecture
+
+
+def lecture_create(request):
+    if request.method == 'POST':
+        form = LectureForm(request.POST)
+        if form.is_valid():
+            lecture = form.save(commit=False)
+            lecture.save()
+            return redirect('lecture:index')
+    else:
+        form = LectureForm()
+    context = {'form': form}
+    return render(request, 'lecture/lecture_create.html', context)
+
+
+def lecture_update(request, lecture_id):
+    lecture = get_object_or_404(Lecture, pk=lecture_id)
+    if request.method == "POST":
+        form = LectureForm(request.POST, instance=lecture)
+        if form.is_valid():
+            lecture = form.save(commit=False)
+            lecture.save()
+            return redirect('lecture:detail', lecture_id=lecture_id)
+    else:
+        form = LectureForm(instance=lecture)
+    context = {'form': form}
+    return render(request, 'lecture/lecture_create.html', context)
+
+
+def lecture_delete(request, lecture_id):
+    lecture = get_object_or_404(Lecture, pk=lecture_id)
+    lecture.delete()
+    return redirect('lecture:index')
