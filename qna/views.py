65f744d01a616d4f69a434ab1ee1944f558c7345
chan <mchank9191@gmail.com>
2021-07-30 16:43:35 +0900

    qna_0730

--- qna/views.py
+++ qna/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
 #from django.http import HttpResponse
 
 # Create your views here.
@@ -25,8 +26,32 @@ def answer_create(request, question_id):
     """
     Q&A 답변등록
     """
-    question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('qna:detail', question_id=question.id)
-# render, redirect 개념 정리 answer_create같은 경우 url이 아닌 int:question이라 다르게 연결 (urls.py참조)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('qna:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'qna/question_detail.html', context)
+    # render, redirect 개념 정리 answer_create같은 경우 url이 아닌 int:question이라 다르게 연결 (urls.py참조)
+
+def question_create(request):
+    """
+    Q&A 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('qna:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'qna/question_form.html', context)
