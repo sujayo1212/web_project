a24082cf5383a73299a0107c8ceb3cb65dcf7d74
kcho32 <kwdc0302@gmail.com>
2021-07-30 16:43:08 +0900

    find_id_added

--- common/urls.py
+++ common/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('find_id/', views.find_id, name='find_id'),
+    # path('reset_pw/', auth_views.PasswordResetView.as_view(), name='reset_pw'),
+    # path('reset_pw/done', auth_views.PasswordResetDoneView, name='reset_done')
 
 ]
