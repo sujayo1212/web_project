233c345305fb4d60cf46a31597d733c4d3aa07fb
sujayo1212 <sujayo1212@gmail.com>
2021-08-03 17:01:42 +0900

    my_page/delete

--- my_page/forms.py
+++ my_page/forms.py
@@ -1,8 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ['email']
+
+class PasswordCheckForm(forms.Form):
+    password = forms.CharField(label='비밀번호', widget=forms.PasswordInput(
+        attrs={'class': 'form-control',})
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        user_password = self.user.password
+
+        if password:
+            if check_password(password, user_password) == False:
+                self.add_error('password', '비밀번호가 일치하지 않습니다.')
