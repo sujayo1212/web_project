409ffb63328ce06e714fe114aa41d81b17f94347
sujayo1212 <sujayo1212@gmail.com>
2021-08-04 17:03:13 +0900

    my_page/join_lecture

--- my_page/urls.py
+++ my_page/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.my_page, name='my_page'),
     path('update/', views.update, name='update'),
     path('my_lecture/', views.my_lecture, name='my_lecture'),
-    path('delete/', views.delete, name='delete')
+    path('delete/', views.delete, name='delete'),
+    path('join_lecture/', views.join_lecture, name='join_lecture'),
 ]
